82fa0bdd156679cd44f2f3fe882092e38ce04d7b
Atenas <lizzmar.idol@gmail.com>
2024-11-05 12:50:11 -0400

    port 80

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 app =Flask(__name__)
 @app.route('/')
 def hola_mundo():
     return render_template('holaflash.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80', debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
