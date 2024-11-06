f0aa142e0f72a97769479917b03e0bef5f131b6b
Atenas <lizzmar.idol@gmail.com>
2024-11-06 13:23:19 -0400

    webhook

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime
+import http.client
 import json
 
 app =Flask(__name__)
@@ -18,13 +19,6 @@ class Log(db.Model):
 #Crear la tabla si no existe
 with app.app_context():
     db.create_all()
-
-    prueba1 = Log(texto='Mensaje de Prueba 1')
-    prueba2 = Log(texto='Mensaje de Prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
 
 #Funcion para ordenar los registros por fecha y hora
 def ordenar_por_fecha_y_hora(registros):
@@ -47,6 +41,75 @@ def agregar_mensajes_log(texto):
     db.session.add(nuevo_registro)
     db.session.commit()
 
+#Token de verificación para la configuración
+TOKEN_GAMLP = "GAMLP"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_GAMLP:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+
+    return jsonify({'message':'EVENT_RECEIVED'})
+    # try:
+    #     req = request.get_json()
+    #     entry =req['entry'][0]
+    #     changes = entry['changes'][0]
+    #     value = changes['value']
+    #     objeto_mensaje = value['messages']
+
+    #     if objeto_mensaje:
+    #         messages = objeto_mensaje[0]
+
+    #         if "type" in messages:
+    #             tipo = messages["type"]
+
+    #             #Guardar Log en la BD
+    #             agregar_mensajes_log(json.dumps(messages))
+
+    #             if tipo == "interactive":
+    #                 tipo_interactivo = messages["interactive"]["type"]
+
+    #                 if tipo_interactivo == "button_reply":
+    #                     text = messages["interactive"]["button_reply"]["id"]
+    #                     numero = messages["from"]
+
+    #                     enviar_mensajes_whatsapp(text,numero)
+                    
+    #                 elif tipo_interactivo == "list_reply":
+    #                     text = messages["interactive"]["list_reply"]["id"]
+    #                     numero = messages["from"]
+
+    #                     enviar_mensajes_whatsapp(text,numero)
+
+    #             if "text" in messages:
+    #                 text = messages["text"]["body"]
+    #                 numero = messages["from"]
+
+    #                 enviar_mensajes_whatsapp(text,numero)
+
+    #                 #Guardar Log en la BD
+    #                 agregar_mensajes_log(json.dumps(messages))
+
+    #     return jsonify({'message':'EVENT_RECEIVED'})
+    # except Exception as e:
+    #     return jsonify({'message':'EVENT_RECEIVED'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
