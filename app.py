24f01d9288866ac8b7bbc4f752c9b422e80fb139
Atenas <lizzmar.idol@gmail.com>
2024-11-06 13:28:05 -0400

    corrección timezine en from

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 import http.client
 import json
 
